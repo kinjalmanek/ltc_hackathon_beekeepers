0a3ddaee36d76c3029a137f73a2917f0118edb95
GirishHaragon <girishharagon115@gmail.com>
2024-07-25 15:37:09 +0530

    Adding Flutter UI Code

---
+++ flex_ui/lib/main.dart
@@ -0,0 +1,103 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flex_users/login.dart';
+import 'package:flex_users/signup.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 120), // Add some space at the top
+              Column(
+                children: <Widget>[
+                  Text(
+                    "Welcome",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 65,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    "XYZ's Flex banking",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.grey[700],
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
+              Spacer(flex: 2), // Add more space between the text and buttons
+              MaterialButton(
+                minWidth: double.infinity,
+                height: 60,
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
+                },
+                color: Color(0xff0095FF),
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(
+                    color: Colors.black,
+                  ),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: Text(
+                  "Login",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 25,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              MaterialButton(
+                minWidth: double.infinity,
+                height: 60,
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => SignupPage()));
+                },
+                color: Color(0xff0095FF),
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(
+                    color: Colors.black,
+                  ),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: Text(
+                  "Sign up",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+              Spacer(flex: 1), // Add more space below the buttons if needed
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

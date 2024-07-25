68d22a498c53f0f780ce708aa4e95c42128d5e92
GirishHaragon <girishharagon115@gmail.com>
2024-07-25 17:51:23 +0530

    Login page Connecting with Http

--- flex_ui/lib/login.dart
+++ flex_ui/lib/login.dart
@@ -1,6 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  Future<void> login() async {
+    final String url = 'https://ltc-hackathon-beekeepers-wct627xdfq-el.a.run.app/signin'; // replace with your backend URL
+    print("Login button pressed");
+    try {
+      final response = await http.post(
+        Uri.parse(url),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{
+          'email': emailController.text,
+          'password': passwordController.text,
+        }),
+      );
+
+      print("Response status: ${response.statusCode}");
+      print("Response body: ${response.body}");
+
+      if (response.statusCode == 200) {
+        if (response.body == 'Success') {
+          // Handle success
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Login successful!')),
+          );
+        } else {
+          // Handle error
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Login failed!')),
+          );
+        }
+      } else {
+        // Handle error
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error: ${response.reasonPhrase}')),
+        );
+      }
+    } catch (e) {
+      print("Exception occurred: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('An error occurred')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,16 +62,16 @@ class LoginPage extends StatelessWidget {
       backgroundColor: Colors.white,
       appBar: AppBar(
         elevation: 0,
-        // brightness: Brightness.light,
         backgroundColor: Colors.white,
         leading: IconButton(
           onPressed: () {
             Navigator.pop(context);
           },
-          icon: Icon(Icons.arrow_back_ios,
-          size: 20,
-          color: Colors.black,),
-
+          icon: Icon(
+            Icons.arrow_back_ios,
+            size: 20,
+            color: Colors.black,
+          ),
         ),
       ),
       body: Container(
@@ -26,113 +80,103 @@ class LoginPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            Expanded(child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    Text("Login",
-                    style: TextStyle(fontSize: 75, fontWeight: FontWeight.bold),),
-                    SizedBox(height: 20),
-                    Text("Login to Flex account",
-                    style: TextStyle(
-                      fontSize: 15,
-                    color:Colors.grey[700]),)
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 40),
-                  child: Column(
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
                     children: <Widget>[
-                      inputFile(label: "Email"),
-                      inputFile(label: "Password", obscureText: true)
+                      Text(
+                        "Login",
+                        style: TextStyle(fontSize: 75, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        "Login to Flex account",
+                        style: TextStyle(fontSize: 15, color: Colors.grey[700]),
+                      ),
                     ],
                   ),
-                ),
-                  Padding(padding:
-                  EdgeInsets.symmetric(horizontal: 40),
-                  child: Container(
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 40),
+                    child: Column(
+                      children: <Widget>[
+                        inputFile(label: "Email", controller: emailController),
+                        inputFile(label: "Password", obscureText: true, controller: passwordController),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 40),
+                    child: Container(
                       padding: EdgeInsets.only(top: 3, left: 3),
-                      decoration:
-                        BoxDecoration(
-                          borderRadius: BorderRadius.circular(50),
-                          border: Border(
-                            bottom: BorderSide(color: Colors.black),
-                            top: BorderSide(color: Colors.black),
-                            left: BorderSide(color: Colors.black),
-                            right: BorderSide(color: Colors.black),
-                          )
-
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        border: Border(
+                          bottom: BorderSide(color: Colors.black),
+                          top: BorderSide(color: Colors.black),
+                          left: BorderSide(color: Colors.black),
+                          right: BorderSide(color: Colors.black),
                         ),
+                      ),
                       child: MaterialButton(
                         minWidth: double.infinity,
                         height: 60,
-                        onPressed: () {},
+                        onPressed: login,
                         color: Color(0xff0095FF),
                         elevation: 0,
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(50),
-
                         ),
                         child: Text(
-                          "Login", style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 18,
-                          color: Colors.white,
-
+                          "Login",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
                         ),
-                        ),
-
                       ),
                     ),
                   ),
-
-              ],
-            ))
+                ],
+              ),
+            ),
           ],
         ),
       ),
     );
   }
-
 }
 
-
 // we will be creating a widget for text field
-Widget inputFile({label, obscureText = false})
-{
+Widget inputFile({required String label, bool obscureText = false, required TextEditingController controller}) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: <Widget>[
       Text(
         label,
-        style: TextStyle(
-          fontSize: 15,
-          fontWeight: FontWeight.w400,
-          color:Colors.black87
-        ),
-
+        style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black87),
       ),
       SizedBox(
         height: 5,
       ),
       TextField(
+        controller: controller,
         obscureText: obscureText,
         decoration: InputDecoration(
-          contentPadding: EdgeInsets.symmetric(vertical: 0,
-          horizontal: 10),
+          contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
           enabledBorder: OutlineInputBorder(
             borderSide: BorderSide(
-              // color: Colors.grey[400]
+              color: Colors.grey,
             ),
-
           ),
-          border: OutlineInputBorder(
-            // borderSide: BorderSide(color: Colors.grey[400])
-          )
+          border: OutlineInputBorder(),
         ),
       ),
-      SizedBox(height: 10,)
+      SizedBox(
+        height: 10,
+      ),
     ],
   );
 }
